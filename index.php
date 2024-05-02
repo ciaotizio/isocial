f65d66a0627ff38fb845cb6d344602a6bfc8f6d8
ciaotizio <ciaotizio59@gmail.com>
2024-05-02 17:08:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram's secret</title>
+</head>
+<body>
+    <?php 
+        echo "COMPLIMENTI HAI ATTIVATO LA MODALITà SEGRETA";
+    ?>
+</body>
+
+</html>
